a22335488154272332ded459b2922820bcfe9be7
SonickSeven <sonickfaber7@yahoo.es>
2013-07-28 22:40:47 -0500

    Create bd.sql

---
+++ bd.sql
@@ -0,0 +1,10 @@
+/**********This is a example with bd*************/
+
+create database ajax;
+use ajax;
+
+create table archivo(
+cod int primary key auto_increment,
+nombre varchar(200),
+nombreArchivo varchar(200)
+);
